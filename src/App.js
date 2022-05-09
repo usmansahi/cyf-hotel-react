ee9d2d00aaef06a496e6f2a72bd4a6fdd966d622
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-05-09 19:22:21 +0200

    homework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Heading from "./components/Heding";
 
 import Bookings from "./components/Bookings";
 import "./App.css";
+import SearchButton from "./components/SerachButton";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">CYF Hotel</header>
+
       <Bookings />
     </div>
   );
